70c9620a69ce1552065b5181825bf052f5c62a6f
terra-earth <120101335+terra-earth@users.noreply.github.com>
2022-12-13 15:22:07 +0700

    Add files via upload

---
+++ jees.js
@@ -0,0 +1,31 @@
+function button1() {
+    window.open('https://github.com/muhammadhanifs');
+}
+
+function button2() {
+    window.open('https://www.instagram.com/_hanifkun');
+}
+
+function button3() {
+    window.open('https://github.com/daniendraputri');
+}
+
+function button4() {
+    window.open('https://instagram.com/danindraputrin');
+}
+
+function button5() {
+    window.open('https://github.com/sadiramd');
+}
+
+function button6() {
+    window.open('https://instagram.com/sadiraamd');
+}
+
+function button7() {
+    window.open('https://github.com/terra-earth');
+}
+
+function button8() {
+    window.open('https://www.instagram.com/zidna19a');
+}
